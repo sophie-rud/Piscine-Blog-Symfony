03eaab49316394a6bca0d16c1164557095fd4e48
sophie-rud <sophie.rudio@lapiscine.pro>
2024-07-23 10:39:32 +0200

    Ajout CategorieController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,52 +15,62 @@ class ArticleController extends AbstractController {
             [
                 'id' => 1,
                 'title' => 'Carapuce',
-                'content' => 'Pokemon eau'
+                'content' => 'Pokemon eau',
+                'isPublished' => true
             ],
             [
                 'id' => 2,
                 'title' => 'Salamèche',
-                'content' => 'Pokemon feu'
+                'content' => 'Pokemon feu',
+                'isPublished' => true
             ],
             [
                 'id' => 3,
                 'title' => 'Bulbizarre',
-                'content' => 'Pokemon plante'
+                'content' => 'Pokemon plante',
+                'isPublished' => true
             ],
             [
                 'id' => 4,
                 'title' => 'Pikachu',
-                'content' => 'Pokemon electrique'
+                'content' => 'Pokemon electrique',
+                'isPublished' => true
             ],
             [
                 'id' => 5,
                 'title' => 'Rattata',
-                'content' => 'Pokemon normal'
+                'content' => 'Pokemon normal',
+                'isPublished' => false
             ],
             [
                 'id' => 6,
                 'title' => 'Roucool',
-                'content' => 'Pokemon vol'
+                'content' => 'Pokemon vol',
+                'isPublished' => true
             ],
             [
                 'id' => 7,
                 'title' => 'Aspicot',
-                'content' => 'Pokemon insecte'
+                'content' => 'Pokemon insecte',
+                'isPublished' => false
             ],
             [
                 'id' => 8,
                 'title' => 'Nosferapti',
-                'content' => 'Pokemon poison'
+                'content' => 'Pokemon poison',
+                'isPublished' => false
             ],
             [
                 'id' => 9,
                 'title' => 'Mewtwo',
-                'content' => 'Pokemon psy'
+                'content' => 'Pokemon psy',
+                'isPublished' => true
             ],
             [
                 'id' => 10,
                 'title' => 'Ronflex',
-                'content' => 'Pokemon normal'
+                'content' => 'Pokemon normal',
+                'isPublished' => false
             ]
 
         ];
